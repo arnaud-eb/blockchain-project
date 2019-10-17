86ee909182b9b4bbe9af21efbfc6b940cddb0bad
arnaud-eb <arnaud.depierreux@easebest.com>
2019-10-17 07:04:19 +0200

    hash(lib) and PoW

--- blockchain.py
+++ blockchain.py
@@ -1,29 +1,45 @@
 import functools
+import hashlib as hl
+from collections import OrderedDict
+
+from hash_util import hash_string_256, hash_block
 
 MINING_REWARD=10
 genesis_block = {
     'previous_hash': '',
     'index': 0,
-    'transactions': []
+    'transactions': [],
+    'proof': 100
 }
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Arnaud'
 participants={'Arnaud'}
 
-def hash_block(block):
-    return "".join([str(value) for value in block.values()])
+
+def valid_proof(transactions, last_hash, proof):
+    guess = (str(transactions) + str(last_hash) + str(proof)).encode()
+    guess_hash = hash_string_256(guess)
+    print(guess_hash)
+    return guess_hash[:2]=='00'
+
+
+def proof_of_work():
+    last_block=blockchain[-1]
+    last_hash=hash_block(last_block)
+    proof = 0
+    while not valid_proof(open_transactions,last_hash,proof):
+        proof+=1
+    return proof
 
 
 def get_balance(participant):
     tx_sender=[[tx['amount'] for tx in block['transactions'] if tx['sender']==participant] for block in blockchain]
     open_tx_sender=[tx['amount'] for tx in open_transactions if tx['sender']==participant]
     tx_sender.append(open_tx_sender)
-    print(tx_sender)
     amount_sent=functools.reduce(lambda tx_sum,tx_amt:tx_sum + sum(tx_amt) if len(tx_amt)>0 else tx_sum + 0,tx_sender,0)
     tx_recipient=[[tx['amount'] for tx in block['transactions'] if tx['recipient']==participant] for block in blockchain]
     amount_received=functools.reduce(lambda tx_sum,tx_amt:tx_sum + sum(tx_amt) if len(tx_amt)>0 else tx_sum + 0,tx_recipient,0)
-    print(tx_recipient)
     return amount_received-amount_sent
 
 
@@ -32,11 +48,7 @@ def verify_transaction(transaction):
 
 
 def add_transaction(recipient, sender=owner, amount=1.0):
-    transaction = {
-        'sender': sender,
-        'recipient': recipient,
-        'amount': amount
-    }
+    transaction=OrderedDict([('sender',sender),('recipient',recipient),('amount',amount)])
     if verify_transaction(transaction):
         open_transactions.append(transaction)
         participants.add(sender)
@@ -44,20 +56,19 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         return True
     return False
 
+
 def mine_block():
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
-    reward_transaction = {
-        'sender': 'MINING',
-        'recipient': owner,
-        'amount': MINING_REWARD
-    }
+    proof = proof_of_work()
+    reward_transaction=OrderedDict([('sender','MINING'),('recipient',owner),('amount',MINING_REWARD)])
     copied_transactions=open_transactions[:]
     copied_transactions.append(reward_transaction)
     block = {
         'previous_hash': hashed_block,
         'index': len(blockchain),
-        'transactions': copied_transactions
+        'transactions': copied_transactions,
+        'proof': proof
     }
     blockchain.append(block)
     return True
@@ -96,6 +107,9 @@ def verify_blockchain():
         if block_index==0:
             continue
         if hash_block(blockchain[block_index-1]) != block['previous_hash']:
+            return False
+        if not valid_proof(block['transactions'][:-1],block['previous_hash'],block['proof']):
+            print('Proof of work is invalid')
             return False
     return True
 
@@ -140,7 +154,7 @@ while waiting_for_input:
     if not verify_blockchain():
         print('Invalid blockchain!')
         break
-    print(f"Balance of {owner}: {get_balance('Arnaud'):6.2f}")
+    print(f"Balance of {owner}: {get_balance(owner):6.2f}")
 else:
     print('User left!')
 
